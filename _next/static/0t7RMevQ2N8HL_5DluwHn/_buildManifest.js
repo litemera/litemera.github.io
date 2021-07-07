120b6af773bb91a4f9308e20047a48f05cea5334
Hyouk Seo <ghsspower@naver.com>
2021-07-07 13:31:04 +0900

    Deploy Next.js to gh-pages

--- _next/static/0t7RMevQ2N8HL_5DluwHn/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-060018d1abdf144221f3.js"],"/_error":["static/chunks/pages/_error-9faf4177fb4e528b4124.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
