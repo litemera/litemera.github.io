7a35d7556b076a8cdfbfb4f2e3d34d6284416b0d
Hyouk Seo <ghsspower@naver.com>
2021-07-07 18:50:36 +0900

    Deploy Next.js to gh-pages

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -55,26 +55,6 @@ class Litemera extends Document {
 
           <link
             rel="apple-touch-icon"
-            sizes="57x57"
-            href="/favicon/apple-icon-57x57.png"
-          />
-          <link
-            rel="apple-touch-icon"
-            sizes="60x60"
-            href="/favicon/apple-icon-60x60.png"
-          />
-          <link
-            rel="apple-touch-icon"
-            sizes="72x72"
-            href="/favicon/apple-icon-72x72.png"
-          />
-          <link
-            rel="apple-touch-icon"
-            sizes="76x76"
-            href="/favicon/apple-icon-76x76.png"
-          />
-          <link
-            rel="apple-touch-icon"
             sizes="114x114"
             href="/favicon/apple-icon-114x114.png"
           />
