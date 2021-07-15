68cd54d1e7f97c9889f6b7432acd99f98051048f
Hyouk Seo <ghsspower@naver.com>
2021-07-15 13:40:09 +0900

    Deploy Next.js to gh-pages

--- utils/constants.ts
+++ utils/constants.ts
@@ -5,7 +5,7 @@ export const DESCRIPTION = `Take a photo with Litemera, it will be deleted autom
 export const META_KEYWORDS = `${TITLE}, Camera, Photo, Image, Picture, App, Application, AppStore, Google Play, Apple, Google, Android, Play Store, Wi-fi, Wifi, Caffe, Cafe, Parking lot, Parking, restroom, toilet, Password, Share, business, lightmera, litecam, lightcam`;
 
 export const IOS_ID = `1575339252`;
-export const IOS_URL = `https://apps.apple.com/kr/app/id${IOS_ID}`;
+export const IOS_URL = `https://apps.apple.com/app/id${IOS_ID}`;
 
 export const ANDROID_PACKAGE = `com.dreammeister.litemera`;
 export const AND_URL = `https://play.google.com/store/apps/details?id=${ANDROID_PACKAGE}`;
